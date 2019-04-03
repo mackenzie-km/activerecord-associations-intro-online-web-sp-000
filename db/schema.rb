e2e59eadd1ebd839437fa2559343f5c67d7b47e0
Mackenzie Moore <macmoore@ucdavis.edu>
2019-04-03 23:34:42 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,13 @@ ActiveRecord::Schema.define(version: 4) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "genres", force: :cascade do |t|
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "name"
     t.integer "artist_id"
     t.integer "genre_id"
   end
